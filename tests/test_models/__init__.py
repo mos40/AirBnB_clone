c2ce4c1f185608bd9fa3adec019ccdeffa774843
Moses <mosesmkhonza@outlook.com>
2024-02-11 10:40:44 -0800

    Refresh

--- tests/test_models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Initialization magic method for models directory"""
-from models.engine.file_storage import FileStorage
-
-
-# Create an instance of FileStorage and load data
-storage_instance = FileStorage()
-storage_instance.reload()
