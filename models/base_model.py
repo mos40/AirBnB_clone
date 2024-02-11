1bd8667e5caeb606b258569626248ae45b639bf2
Moses <mosesmkhonza@outlook.com>
2024-02-11 13:02:46 -0800

    modes

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,7 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format
-    (self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
